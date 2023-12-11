2e4411c0d25f8cc515c7b5a49ab4861b7d5ddc5f
devmianharoon <devmianharoon@gmail.com>
2023-12-11 15:05:30 +0500

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,9 @@ export default function Home() {
     <div>
       <h1 className="font-bold text-lg mt-10 mx-10">Develop a Next js app and use states to ahow the name of a person from a list and on click of button it should print next or previous name from the list</h1>
       <div className=" mt-10 mx-10 flex gap-14 justify-center items-center ">
-        <button className="border-[3px] font-mono font-bold px-3 py-2 bg-orange-500 rounded-lg" onClick={incrementHandler}>Next</button>
+      <button className="border-[3px] font-mono font-bold px-3 py-2 bg-orange-500 rounded-lg" onClick={decrementHandler}>Prev</button>
         <h1 className="font-mono font-bold text-lg  text-center">{name}</h1>
-        <button className="border-[3px] font-mono font-bold px-3 py-2 bg-orange-500 rounded-lg" onClick={decrementHandler}>Prev</button>
+        <button className="border-[3px] font-mono font-bold px-3 py-2 bg-orange-500 rounded-lg" onClick={incrementHandler}>Next</button>
       </div>
     </div>
   )
